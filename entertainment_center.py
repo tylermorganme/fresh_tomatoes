1e99a9c64ac145c71a9c052755ca7339a174d9de
Tyler Morgan <tylermorgan@Tylers-MacBook-Pro.local>
2015-05-10 13:36:45 -0600

    Initial commit. Added extra fields to Movies and additonal markup.

---
+++ entertainment_center.py
@@ -0,0 +1,35 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A movie about toys coming to life",
+                        "G",
+                        "John Lasseter",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc");
+
+princess_mononoke = media.Movie("Princess Mononoke",
+                                "A young man and a feral woman must save the world.",
+                                "PG-13",
+                                "Hayao Miyazaki",
+                                "http://ia.media-imdb.com/images/M/MV5BMjgzNTUwODQzN15BMl5BanBnXkFtZTcwMTc4ODE3OQ@@._V1_SX214_AL_.jpg",
+                                "https://www.youtube.com/watch?v=4OiMOHRDs14")
+
+con_air = media.Movie("Con Air",
+                     "After a violent highjacking, a convict must help the police.",
+                     "R",
+                     "Simon West",
+                     "http://ia.media-imdb.com/images/M/MV5BMTU1NzY2NjIzNV5BMl5BanBnXkFtZTgwMzE2Mzk5MDE@._V1_SY317_CR0,0,214,317_AL_.jpg",
+                     "https://www.youtube.com/watch?v=fWq-S1_1vnc")
+
+shawshank_redemption = media.Movie("The Shawshank Redemtion",
+                                   "Life in prison isn't all it's cracked up to be.",
+                                   "R",
+                                   "Frank Darabont",
+                                   "http://ia.media-imdb.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX214_AL_.jpg",
+                                   "https://www.youtube.com/watch?v=6hB3S9bIaco");
+
+movies= [toy_story, princess_mononoke, con_air, shawshank_redemption]
+
+fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.__doc__)
---
+++ media.py
@@ -0,0 +1,26 @@
+import webbrowser
+
+class Movie():
+    """This class stores data for movies"""
+    
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 movie_rating,
+                 movie_director,
+                 poster_image,
+                 trailer_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.rating = movie_rating
+        self.director = movie_director
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube_url
+        
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+    
+        
+        
+
+        
